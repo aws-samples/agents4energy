987b8cb30fb56b79351f9f3fcd277424eca21820
Walt Mayfield <waltmayf@amazon.com>
2025-03-05 17:06:48 -0600

    updated configured agents

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -46,7 +46,6 @@ export const defaultAgents: { [key: string]: BaseAgent | BedrockAgent | LangGrap
             "In September 2024, what are a few key incidents and actions taken at the biodiesel unit?",
         ],
     } as BedrockAgent,
-<<<<<<< HEAD
     // ProductionAgent: {
     //     name: "Production Agent",
     //     source: "graphql",
@@ -64,7 +63,6 @@ export const defaultAgents: { [key: string]: BaseAgent | BedrockAgent | LangGrap
     //         "What portion of world oil production does the US produce?"
     //     ]
     // }
-=======
     RegulatoryAgent: {
         name: "Regulatory Agent",
         source: "bedrockAgent",
@@ -75,36 +73,6 @@ export const defaultAgents: { [key: string]: BaseAgent | BedrockAgent | LangGrap
             "What are the requirements for decomissioning an offshore oil well in Brazil?",
         ],
     } as BedrockAgent,
-    ProductionAgent: {
-        name: "Production Agent",
-        source: "graphql",
-        invokeFieldName: "invokeProductionAgent",
-        samplePrompts: [
-            `Search the well files for the well with API number 30-045-29202 to make a table with type of operation (drilling, completion, workover, plugging, other), text from the report describing operational details, and document title.
-            Also execute a sql query to get the total monthly oil, gas and water production from this well.
-            Create a plot with both the event data and the production data. `.replace(/^\s+/gm, ''), //This trims the white space at the start of each line
-            `Plot the total monthly oil, gas, and water production since 1900 for the well with API number 30-045-29202`
-        ]
-    },
-    FoundationModel: {
-        name: "Foundation Model",
-        samplePrompts: [
-            "What portion of world oil production does the US produce?"
-        ]
-    },
-    PlanAndExecuteAgent: {
-        name: "Plan And Execute",
-        samplePrompts: [
-            `This morning well with API number 30-045-29202 stopped producing gas with indication of a hole in tubing.  
-            Make a table of all operational events found in the well files. 
-            Query all historic monthly production rates and make a plot with both the event and production data. 
-            Estimate the value of the well's remaining production. 
-            Write a procedure to repair the well, estimate the cost of the repair, and calculate financial metrics. 
-            Make an executive report about repairing the well with detailed cost and procedure data. 
-            Use the ai role for all steps.
-            `.replace(/^\s+/gm, ''),
-            `Where should I go on vacation?`
-        ]
-    },
->>>>>>> main
+    
+    
 }
